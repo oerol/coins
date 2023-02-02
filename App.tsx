ed35858c5480949f74bc826f28ba522ba5122740
Özgür Erol <erolxerolx@gmail.com>
2023-02-02 10:09:55 +0100

    Created a day component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Day from './components/Day';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Day/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: 70,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ components/Day.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { LayoutChangeEvent, StyleSheet, Text, View } from "react-native";
+
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+const currentMonth = currentDate.getMonth();
+const currentDay = currentDate.getDate();
+const currentMonthString = currentDate
+  .toLocaleString("default", { month: "long" })
+  .toUpperCase();
+const numberOfDays = new Date(currentYear, currentMonth + 1, 0).getDate(); // Returns the last day of the current month
+
+export default function Day() {
+  const [progressWidth, setProgressWidth] = useState(0);
+
+  const getProgress = (progressBarElementWidth: number) => {
+    return (currentDay / numberOfDays) * progressBarElementWidth;
+  };
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    let progressBarElementWidth = event.nativeEvent.layout.width;
+    setProgressWidth(getProgress(progressBarElementWidth));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        {currentDay}th of {currentMonthString} {currentYear}
+      </Text>
+      <View onLayout={(e) => onLayout(e)} style={styles.progressBar} />
+      <View style={[styles.progressBarFilled, { width: progressWidth }]} />
+    </View>
+  );
+}
+
+const progressBarHeight = 2;
+
+const styles = StyleSheet.create({
+  container: {},
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  progressBar: {
+    height: progressBarHeight,
+    backgroundColor: "#959C83",
+  },
+  progressBarFilled: {
+    transform: [{ translateY: -progressBarHeight }],
+    height: progressBarHeight,
+    backgroundColor: "#C898F8",
+  },
+});
