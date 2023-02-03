4f12f5af0b65b36e9b0a0dd6d70f5dd935853751
Özgür Erol <erolxerolx@gmail.com>
2023-02-03 10:55:18 +0100

    Upon entry creation, the background will be grayed out

--- components/EntryCreation.tsx
+++ components/EntryCreation.tsx
@@ -1,29 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-import {Dimensions} from 'react-native';
-import { useKeyboard } from './Keyboard';
+import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+import { useKeyboard } from "./Keyboard";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
-const entryCreationHeight = 200
+const entryCreationHeight = 250;
 
 export default function EntryCreation() {
   const keyboardHeight = useKeyboard();
-  
+
   return (
-    <View style={[styles.container, { top: windowHeight - keyboardHeight - entryCreationHeight}]}>
+    <View style={styles.overlay}>
       <Text style={styles.text}></Text>
+      <View
+        style={[styles.container, { top: windowHeight - keyboardHeight - entryCreationHeight }]}
+      ></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  overlay: {
+    position: "absolute",
+    width: windowWidth,
+    height: windowHeight,
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    borderTopStartRadius: 30,
+    borderTopEndRadius: 30,
+  },
   container: {
     position: "absolute",
     width: windowWidth,
     height: entryCreationHeight,
-    // left: -20, // App.tsx: padding
-    backgroundColor: '#5ca5cc',
+    backgroundColor: "#281340",
+    borderTopStartRadius: 30,
+    borderTopEndRadius: 30,
   },
   text: {},
 });
